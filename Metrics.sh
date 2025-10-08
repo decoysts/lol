8f2f0b097bfc5070d5e8db6215ad48e763b8ed25
decoysts <132171659+decoysts@users.noreply.github.com>
2025-10-08 09:23:46 +0300

    Update Metrics.sh

--- Metrics.sh
+++ Metrics.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
-
-# Изменение значения hdddisk
-hdddisk="/dev/sdb1"
-hdd=$hdddisk  # change /dev/sdb1 to your value
+connect="mysql -u user -p1"
+hdddisk="/dev/mapper/centos-root" # change /dev/sdb1 to your value
 date=$(date +"%H:%M:%S")
 
-# Проверка метрик
-checkRamUnits=$(free -h | grep Mem | awk '{print $7}')
-checkHddUnits=$(df -h | grep $hdd | awk '{print $4}')
-
-# Обработка RAM
-if [[ $checkRamUnits == "0" ]]; then
-    ram=$(free -h | grep Mem | awk '{print $7}') sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
+#ScanMetrics
+checkRamUnits=$(free -h | grep Mem | awk '{print $7}') | grep G | wc -l
+if [[ $checkRamUnits == 1 ]]; then ram=$(free -h | grep Mem | awk '{print $7}') | sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
 else
-    ram=$(free -h | grep Mem | awk '{print $7}') sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
+    ram=$(free -h | grep Mem | awk '{print $7}') | sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
 fi
 
-# Обработка HDD
-if [[ $checkHddUnits == "0" ]]; then
-    hdd=$(df -h | grep $hdd | awk '{print $4}') sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
+checkHddUnits=$(df -h | grep $hdd | awk '{print $4}') | grep G | wc -l
+if [[ $checkHddUnits == 1 ]]; then hdd=$(df -h | grep $hdd | awk '{print $4}') | sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
 else
-    hdd=$(df -h | grep $hdd | awk '{print $4}') sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
+    hdd=$(df -h | grep $hdd | awk '{print $4}') | sed 's/[^0-9.]//g' | sed -r 's/([0-9]+\.[0-9])[GM]/\1/'
 fi
 
-# Обработка CPU
-freecpu=$(top -n 1 | grep %Cpu | awk '{print $8}')
+freecpu=$(top -n 1 | grep %Cpu | awk '{print $8}') | sed -r 's/[^0-9.]//g'
 cpu=$((100 - $freecpu))
 
-# Обработка статуса
 #DataProcessing
-if [[ $checkRamUnits == "0" ]]; then ramStatus="low memory"; fi
-if [[ $checkHddUnits == "0" ]]; then hddStatus="low hdd"; fi
+if [[ $checkRamUnits == 0 ]]; then ramStatus="low memory"; fi
+if [[ $checkHddUnits == 0 ]]; then hddStatus="low hdd"; fi
 if [[ $cpu > 89 ]]; then cpuStatus="high CPU"; fi
 
 status="${ramStatus} ${hddStatus} ${cpuStatus}"
@@ -38,16 +29,17 @@ status="${ramStatus} ${hddStatus} ${cpuStatus}"
 checkStatus=$(echo $status | wc -m)
 if [[ $checkStatus < 10 ]]; then status="OK"; fi
 
-# Запрос в БД
+#Query in DB
 echo "INSERT INTO servers (ram, hdd, cpu, status) VALUES ('$ram', '$hdd', '$cpu', '$status');"
 
-# Вывод в консоль
+#View in console
 echo $ram
 echo $hdd
 echo $cpu
 echo $status
 
-# Лог
-echo "$date >> /scripts/metrics.log"
+#Log journal
+date >> /scripts/metrics.log
+
 chmod +x metrics.sh
 touch metrics.log
